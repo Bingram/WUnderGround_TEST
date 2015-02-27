2308fcb56c8b011f96ff1567e3f335ccdd54d6f6
Brian <ingram.brian@gmail.com>
2015-02-26 17:57:11 -0800

    Minor Change...nothing to see here

---
+++ src/DataStructures/BoundaryTest.java
@@ -0,0 +1,35 @@
+package DataStructures;
+
+import junit.framework.TestCase;
+
+public class BoundaryTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetQuads() throws Exception {
+
+    }
+
+    public void testGetMyPoints() throws Exception {
+
+    }
+
+    public void testGetMyCenter() throws Exception {
+
+    }
+
+    public void testGetMyName() throws Exception {
+
+    }
+
+    public void testGetMyRadius() throws Exception {
+
+    }
+}
