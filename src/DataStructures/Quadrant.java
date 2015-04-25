16deed28f3f7378f94734e513965cca84c618735
Brian <ingram.brian@gmail.com>
2015-04-25 12:39:50 -0700

    Created Quadrant structure to contain edges and levels for each area of a given user boundary.

--- src/DataStructures/Quadrant.java
+++ src/DataStructures/Quadrant.java
@@ -5,13 +5,48 @@ package DataStructures;
  */
 public class Quadrant {
 
+    private PointList level0,level1,level2,level3;
+
     /**
-     * Data structure to contain the different levels in a given
-     * quadrant/area of a Boundary object.
-     *
-     * FIELDS
-     * Levels
-     *      LinkedList(s) of Points
-     *
+     * Quadrant contains different levels of boundary edges
+     * These edges are a PointList, with level 0 being the outer most
+     * boundary edge, and level 3 being the inner most edge.
      */
+    public Quadrant(){
+        level0=level1=level2=level3= new PointList();
+    }
+
+    public PointList getLevel0() {
+        return level0;
+    }
+
+    public void setLevel0(PointList level0) {
+        this.level0 = level0;
+    }
+
+    public PointList getLevel1() {
+        return level1;
+    }
+
+    public void setLevel1(PointList level1) {
+        this.level1 = level1;
+    }
+
+    public PointList getLevel2() {
+        return level2;
+    }
+
+    public void setLevel2(PointList level2) {
+        this.level2 = level2;
+    }
+
+    public PointList getLevel3() {
+        return level3;
+    }
+
+    public void setLevel3(PointList level3) {
+        this.level3 = level3;
+    }
+
+
 }
