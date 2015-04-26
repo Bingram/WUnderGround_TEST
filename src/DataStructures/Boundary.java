5c67f1127a24644e3b9aa123ec80360352ce722a
Brian <ingram.brian@gmail.com>
2015-04-25 17:26:09 -0700

    Created Quadrant structure to contain edges and levels for each area of a given user boundary.

--- src/DataStructures/Boundary.java
+++ src/DataStructures/Boundary.java
@@ -8,12 +8,13 @@ public class Boundary {
 
     private final Point myCenter;
     private final int myRadius;
+    private int myPointCount;
 
     private final String myName;
 
-    private PointList[] myQuads = new PointList[8];//quadrants of the boundary
+    private Quadrant[] myQuads = new Quadrant[8];//quadrants of the boundary
 
-    private PointList myPoints = new PointList();//all the points contained in a circle
+    //private PointList myPoints = new PointList();//all the points contained in a circle
 
     /**
      * Blank Boundary with default values
@@ -62,7 +63,7 @@ public class Boundary {
      */
     private void quadSetup(){
         for (int i = 0; i < 8; i++) {
-            myQuads[i] = new PointList();
+            myQuads[i] = new Quadrant();
         }
     }
 
@@ -88,14 +89,14 @@ public class Boundary {
 
             //Creates a clockwise order of points
             //relative to position
-            myQuads[0].addLIFO(p0);
-            myQuads[1].addLIFO(p1);
-            myQuads[2].addLIFO(p2);
-            myQuads[3].addLIFO(p3);
-            myQuads[4].addLIFO(p4);
-            myQuads[5].addLIFO(p5);
-            myQuads[6].addLIFO(p6);
-            myQuads[7].addLIFO(p7);
+            myQuads[0].getLevel0().addLIFO(p0);
+            myQuads[1].getLevel0().addLIFO(p1);
+            myQuads[2].getLevel0().addLIFO(p2);
+            myQuads[3].getLevel0().addLIFO(p3);
+            myQuads[4].getLevel0().addLIFO(p4);
+            myQuads[5].getLevel0().addLIFO(p5);
+            myQuads[6].getLevel0().addLIFO(p6);
+            myQuads[7].getLevel0().addLIFO(p7);
 
             if (d < 0) {
                 d += 2 * x + 1;
